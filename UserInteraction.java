18280a615f7fe671130f9e7c47e79534f170dbf1
Julia Lima <134069825+limaakkjkj@users.noreply.github.com>
2024-06-17 17:11:33 -0300

    Add files via upload

---
+++ UserInteraction.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class UserInteraction {
+    public static void main(String[] args) {
+        String status = "Até agora nada aconteceu...";
+        try {
+            Scanner scnUsuario = new Scanner(System.in);
+            Scanner scnSenha = new Scanner(System.in);
+            System.out.println(" Digite o usuario e tecle Enter ");
+            String txtUsuario = scnUsuario.nextLine();
+            System.out.println(" Digite a senha e tecle Enter ");
+            String txtSenha = scnSenha.nextLine();
+
+            if( txtUsuario.equals("JuliaLima") && txtSenha.equals("1234")){
+                status = "Usuario logado com sucesso!";
+            }
+            scnUsuario.close();
+            scnSenha.close();
+
+        } catch (Exception e) {
+            status = "Erro Exception: " + e;
+        }
+        System.out.println(status);
+    }
+
+}
